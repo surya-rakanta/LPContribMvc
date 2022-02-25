8aabcb154079bdf9f5d9a53d91b351a9457b514a
Eka Siswanto <surya_rakanta@yahoo.com>
2022-02-25 08:07:27 +0700

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyConfiguration("release")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyCompany("MVC Contrib")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyCopyright("Copyright © MVC Contrib 2007-2012")]
+[assembly: AssemblyProduct("LPContribMvc")]
+[assembly: AssemblyFileVersion("3.0.100.0")]
+[assembly: AssemblyInformationalVersion("3.0.100.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.0.100.0")]
